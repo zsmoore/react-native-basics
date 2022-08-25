3d29a2b647efd05d625f8ce5e5eb2bd0fdd802c1
zsmoore <zsmoore@zachary-moore.com>
2022-08-25 17:18:21 -0400

    State

--- first-project/lib/Button.tsx
+++ first-project/lib/Button.tsx
@@ -1,4 +1,5 @@
 import { Button as ReactButton } from "react-native"
+import { useState, useEffect } from "react"
 
 interface ButtonProps {
   buttonText: string
@@ -6,7 +7,15 @@ interface ButtonProps {
 }
 
 export default function Button(props: ButtonProps) {
+  const [count, setCount] = useState(0);
+  const [buttonCountText, setButtonCountText] = useState(`I was pressed ${count} times`);
+  useEffect(() => {
+    setButtonCountText(`I was pressed ${count} times!`);
+  }, [count]);
+
   return (
-    <ReactButton title={props.buttonText} onPress={props.onClick != null ? props.onClick : () => {}}/>
+    <ReactButton 
+     title={props.buttonText + buttonCountText}
+     onPress={props.onClick != null ? props.onClick : () => {setCount(count + 1)}}/>
   )
 }

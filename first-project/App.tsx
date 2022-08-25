c983366780a550540709396a932d0702b9f8c6a0
zsmoore <zsmoore@zachary-moore.com>
2022-08-25 16:44:05 -0400

    Wire up action

--- first-project/App.tsx
+++ first-project/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Button  from './lib/Button';
 
 export default function App() {
--- first-project/lib/Button.tsx
+++ first-project/lib/Button.tsx
@@ -2,10 +2,11 @@ import { Button as ReactButton } from "react-native"
 
 interface ButtonProps {
   buttonText: string
+  onClick?: () => void
 }
 
 export default function Button(props: ButtonProps) {
   return (
-    <ReactButton title={props.buttonText}/>
+    <ReactButton title={props.buttonText} onPress={props.onClick != null ? props.onClick : () => {}}/>
   )
 }
